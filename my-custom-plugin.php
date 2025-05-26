a672057f43ebebbe19cc32ee25246ccccc9332b4
arossi58 <31297871+arossi58@users.noreply.github.com>
2025-05-26 08:16:14 -0500

    Update my-custom-plugin.php

--- my-custom-plugin.php
+++ my-custom-plugin.php
@@ -1,11 +1,12 @@
 <?php
 /*
-Plugin Name: My Custom Plugin
-Description: Site-specific PHP utilities.
+Plugin Name: AIGA KC Custom Plugin
+Description:  All of our site-specific PHP utilities for AIGA KC.
 Version:     1.0.0
-Author:      Your Name
+Author:      Andrew Rossi
 */
-// Autoload everything in /includes
-foreach ( glob( __DIR__ . '/includes/*.php' ) as $file ) {
+
+// then pull in all your php/*.php modules:
+foreach ( glob( __DIR__ . '/php/*.php' ) as $file ) {
   require_once $file;
 }
