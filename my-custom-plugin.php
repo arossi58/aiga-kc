08ce9798df752aaf255804bfb29006195df5fe46
arossi58 <31297871+arossi58@users.noreply.github.com>
2025-05-26 19:07:07 -0500

    Update my-custom-plugin.php

--- my-custom-plugin.php
+++ my-custom-plugin.php
@@ -1,11 +1,11 @@
 <?php
-
+/*
 Plugin Name: AIGA KC Custom Plugin
 Description:  All of our site-specific PHP utilities for AIGA KC.
 Version:     1.0.1
 Author:      Andrew Rossi
 
-
+*/
 // then pull in all your php/*.php modules:
 foreach ( glob( __DIR__ . '/php/*.php' ) as $file ) {
   require_once $file;
